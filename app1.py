457a25abff77d9934bdc9bf4dca4c511d07d59bb
sruju003 <80395231+sruju003@users.noreply.github.com>
2024-05-31 14:10:54 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,39 @@
+# app.py
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+
+
+# dictionaries.py
+dict1 = {1: ['IfcMeasure', 'IfcPerson', 'IfcGeometricR.SubContext', 'IfcProject', 'IfcOwnerHistory', 
+             'IfcPersonAndOrganization', 'IfcSIUnit', 'IfcConversionBasedUnit', 'IfcPostalAddress', 
+             'IfcGeometricR.Context', 'IfcLocal', 'IfcSite', 'IfcApplication', 'IfcUnitAssignment', 
+             'IfcAxis2Placement3D', 'IfcBuilding', 'IfcBuildingStorey', 'IfcOrganization', 
+             'IfcDimensionalExponents', 'IfcCartesianPoint']}
+combined_list = list(set(sum(dict1.values(), [])))
+
+
+# Path to the IFC file
+IFC_FILE_PATH = r'C:\Users\SRUJANA GOLLA\Desktop\IISc\prajwal\3D-BIM-main\3D-BIM-main\episode-09\models\mad_scientist_212.ifc'
+
+@app.route('/')
+def index():
+    return render_template('index3.html', names=combined_list)
+
+@app.route('/search', methods=['POST'])
+def search():
+    selected_keywords = [keyword.lower() for keyword in request.form.getlist('keywords')]
+    found_lines = []
+
+    # Search the IFC file for selected keywords
+    with open(IFC_FILE_PATH, 'r') as f:
+        for line in f:
+            if any(keyword in line.lower() for keyword in selected_keywords):
+                found_lines.append(line.strip())
+
+    return render_template('results.html', lines=found_lines, keywords=selected_keywords)
+
+if __name__ == '__main__':
+    app.run(debug=True)
